bbaf8c425328f53abe286323b0c18f868986ed3f
jfarrow <jfarrow@deloitte.com>
2017-08-01 22:34:33 -0400

    BUGFIX: makeDateTimeString function returns properly-formatted months

--- scripts/meetings.js
+++ scripts/meetings.js
@@ -20,34 +20,58 @@ APP.meetings = (  function(  global, app  ){
     Nov    :     "11",
     Dec    :     "12"
 
-  }
+  },
+
+    fullMonthMap = {
+      January     :     "01",
+      February    :     "02",
+      March       :     "03",
+      April       :     "04",
+      May         :     "05",
+      June        :     "06",
+      July        :     "07",
+      August      :     "08",
+      September   :     "09",
+      October     :     "10",
+      November    :     "11",
+      December    :     "12"
+    }
+
+
 
   function makeDateTimeString(  dateStr, timeStr  ){
     var dateTimeString,
         dateTimeArr = [],
-        propNames = Object.getOwnPropertyNames(  monthMap  ),
-        newMonth;
+        propNames = Object.getOwnPropertyNames(  monthMap  );
+        fullPropNames = Object.getOwnPropertyNames(  fullMonthMap  );
         dateTimeString = dateStr.replace(  /,\s/, ' '  );
         dateTimeString = dateTimeString.split(  ' '  );
         dateTimeString = dateTimeString[  2  ] + '.' + dateTimeString[  1  ] + '.' + dateTimeString[  0  ] + '.';
         dateTimeString = dateTimeString + timeStr;
-
-        /**TODO: Refactor to use the dateTimeString ARRAY instead
-        (fina a way to use as the 'arguments' attribute to Date.UTC?)
-        */
         dateTimeString = dateTimeString.split(  '.'  );
-        for(  var i = 0; i < propNames.length; i++  ){
+        for(  var i = 0; i < 13; i++  ){
           if(  dateTimeString[  1  ] === propNames[  i  ]  ){
             dateTimeString[  1  ] = monthMap[  propNames[  i  ]  ];
           }
+          if( dateTimeString[  1  ]  === fullPropNames[  i  ] ){
+            dateTimeString[  1  ] = fullMonthMap[  fullPropNames[  i  ]  ];
+          }
         }
 
-        //console.log(  dateTimeString  );
-        // dateTimeString = dateTimeString.replace(  /,/,  ' '  );
-        // dateTimeString = dateTimeString.replace(  /,/, ' '  );
-        // dateTimeString = dateTimeString.replace(  /,/, ' '  );
+        console.log(  '************************************************'  );
+        console.log(  dateTimeString  );
 
         for(  var j = 0; j < dateTimeString.length; j++  ){
+          // for( var k = 0; k < propNames.length; k++  ){
+          //   if( (  dateTimeString[  j  ]  ).substr(  0, 3  ) === propNames[  k  ]  ){
+          //     dateTimeString[  j  ] = monthMap[  k  ];
+          //   }
+          // }
+          // for( var month in monthMap  ){
+          //   if(  dateTimeString[  j  ].substr(  0, 3  ) === Object.getOwnPropertyNames(  monthMap  )  ){
+          //     dateTimeString[  j  ] = monthMap[  month  ];
+          //   }
+          // }
           if(  j !== dateTimeString.length -1  ){
             dateTimeArr.push(  dateTimeString[  j  ]  );
             //console.log(  "dateTimeArr: " + dateTimeArr  );
@@ -58,19 +82,24 @@ APP.meetings = (  function(  global, app  ){
             var hrsMins = dateTimeString[  j  ].split(  ':'  );
             var hrs = hrsMins[  0  ];
             var mins = hrsMins[  1  ];
-            mins = parseInt(  mins  );
+            //mins = parseInt(  mins  );
             dateTimeArr.push(  hrs  );
             dateTimeArr.push(  mins  );
           }
         }
         //Parse dateTimeArr values into INTEGERS for conversion
         //to UTC
-        for(  var k = 0; k < dateTimeString.length; k++  ){
-          dateTimeArr[  k  ] = parseInt(  dateTimeArr[  k  ]  );
-        }
+        // for(  var k = 0; k < dateTimeString.length; k++  ){
+        //   dateTimeArr[  k  ] = parseInt(  dateTimeArr[  k  ]  );
+        // }
         //console.dir(  dateTimeArr  );
         return dateTimeArr;
   }
+
+  // function makeDateTimeString( yr, mth, day, hr, min ){
+  //   var dateTime = yr + '-' + mth + '-' + day + 'T' + hr + ':' + min + ':' + '00.000';
+  //   return dateTime;
+  // }
 
   function addMeeting(){
     //console.log(  'Web URL: ' + app.getWebUrl()  ); -->OK
@@ -114,22 +143,16 @@ APP.meetings = (  function(  global, app  ){
         /****Begin build UTC now date********/
         utcDateStamp = new Date(  Date.now()  );
         utcDateStamp = utcDateStamp.toString();
-        console.log(  utcDateStamp  );
-        console.log(  '**************************************');
+        //console.log(  utcDateStamp  );
         utcDateStamp = utcDateStamp.split(  ' '  );
-        console.log(  '**************************************');
-        console.log(  utcDateStamp  );
+        //console.log(  utcDateStamp  );
         yearNow = utcDateStamp[  3  ];
-        //
-        console.log(  yearNow  );
-        console.log(  '**************************************');
         for(  var month in monthMap  ){
           if(  utcDateStamp[  1  ] === month  ){
             monthNow = monthMap[  month  ];
           }
         }
-        console.log(  monthNow  );
-        console.log(  '**************************************');
+        //console.log(  monthNow  );
         dayNow = utcDateStamp[  2  ];
         hrMinNow = utcDateStamp[  4  ];
         //utcDateStamp = yearNow + monthNow + dayNow + hrMinNow;
@@ -150,11 +173,13 @@ APP.meetings = (  function(  global, app  ){
         /****Begin build UTC start date******/
         dateStartVal = makeDateTimeString(  dateStartVal, timeStartVal  );
         startYear = dateStartVal[  0  ];
-        startMonth = dateStartVal[  1  ];
+        //startMonth = dateStartVal[  1  ];
         startDay = dateStartVal[  2  ];
         startHour = dateStartVal[  3  ];
         startMinute = dateStartVal[  4  ];
-        utcDateStart = Date.UTC(  startYear, startMonth, startDay, startHour, startMinute  );
+        //utcDateStart = Date.UTC(  startYear, startMonth, startDay, startHour, startMinute  );
+        utcDateStart = startYear + '-' + startMonth + '-' + startDay + 'T' + startHour +
+          ':' + startMinute + ':' + '00.000';
         console.log(  'UTC Date Start: ' + utcDateStart  );
         /****End build UTC start date******/
 
@@ -169,24 +194,24 @@ APP.meetings = (  function(  global, app  ){
         console.log(  'UTC Date End: ' + utcDateEnd  );
         /****End build UTC end date*****/
 
-    $().SPServices(
-      {
-        operation         :     'AddMeeting',
-        webURL            :     webURL,
-        uid               :     uid,
-        organizerEmail    :     organizerEmail,
-        sequence          :     sequence,
-        utcDateStamp      :     utcDateStamp,
-        title             :     titleVal,
-        utcDateStart      :     utcDateStart,
-        utcDateEnd        :     utcDateEnd,
-        nonGregorian      :     false,
-        completefunc      :     function(  xData, Status  ){
-          console.log(  Status  );
-          console.log(  xData.responseText  );
-        }
-      }
-    );
+    // $().SPServices(
+    //   {
+    //     operation         :     'AddMeeting',
+    //     webURL            :     webURL,
+    //     uid               :     uid,
+    //     organizerEmail    :     organizerEmail,
+    //     sequence          :     sequence,
+    //     utcDateStamp      :     utcDateStamp,
+    //     title             :     titleVal,
+    //     utcDateStart      :     utcDateStart,
+    //     utcDateEnd        :     utcDateEnd,
+    //     nonGregorian      :     false,
+    //     completefunc      :     function(  xData, Status  ){
+    //       console.log(  Status  );
+    //       console.log(  xData.responseText  );
+    //     }
+    //   }
+    // );
     /*
     *SPServices 'AddMeeting' method parameters:
       organizerEmail        :     Email address ('email_address@domain.ext') of the organizer
