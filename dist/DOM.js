20a0b06428dd338cc7e28cf6c095c67206d55543
jfarrow <jfarrow@deloitte.com>
2017-08-03 15:49:22 -0400

    DOM.js

---
+++ dist/DOM.js
@@ -0,0 +1,58 @@
+/*
+*Filename       :       DOM.js
+*Create Date    :       08-03-2017 11:19 EST
+*Author         :       jFarrow02
+*Purpose        :       App module for DOM creation/manipulation functions
+*
+*/
+
+APP.dom = (  function(  window  ){
+
+  function buildOverlay(  parent, child  ){
+    var overlay = document.createElement(  'div'  );
+
+    //Set position attribute of parent element to 'relative' for positioning of overlay
+    parent.style.position = 'relative';
+
+    //Set positiion and attributes of overlay
+    overlay.setAttribute(  'id', "overlay"  );
+    overlay.style.position = 'absolute';
+    overlay.style.backgroundColor = 'rgba(51, 51, 51, 0.6)';
+    overlay.style.width = window.innerWidth + 'px';
+    overlay.style.height = window.innerHeight + 'px';
+    overlay.style.top = window.pageYOffset + 'px';
+    overlay.style.left = window.pageXOffset + 'px';
+    overlay.style.zIndex = '200';
+
+    window.addEventListener(  'resize', function(){
+      if(  overlay  ){
+        overlay.style.width = window.innerWidth + 'px';
+        overlay.style.height = window.innerHeight + 'px';
+        overlay.style.top = window.pageYOffset + 'px';
+        overlay.style.left = window.pageXOffset + 'px ';
+      }
+    }, false);
+
+    window.addEventListener(  'scroll', function(){
+      if(  overlay  ){
+        overlay.style.width = window.innerWidth + 'px';
+        overlay.style.height = window.innerHeight + 'px';
+        overlay.style.top = window.pageYOffset + 'px';
+        overlay.style.left = window.pageXOffset + 'px ';
+      }
+    }, false  );
+
+    //Append overlay to parent
+    parent.appendChild(  overlay  );
+    if(  child  ){
+      overlay.appendChild(  child  );
+    }
+  }
+
+  //Return public API
+  return(
+    {
+      buildOverlay          :         buildOverlay
+    }
+  )
+}  )(  this  );
