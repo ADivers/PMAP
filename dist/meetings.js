48b9c45fb5916c354532a8a88bd5e49ec77b10a8
jfarrow <jfarrow@deloitte.com>
2017-08-03 11:11:18 -0400

    Successfully hits SP workflow and sends auto-email

--- dist/meetings.js
+++ dist/meetings.js
@@ -139,9 +139,12 @@ APP.meetings = (  function(  global, app  ){
                     $(  xData.responseXML  ).find(  'Fields  > Field'  ).each(  function(){
                       var $node = $(  this  );
                       console.log(  'Type: ' + $node.attr(  'Type'  )  + ' StaticName: ' + $node.attr(  'StaticName'  )  );
-                      window.Location.reload(  true  );
                       //document.querySelectorAll(  '#create-meeting[input]'  )
                     }  );
+                    console.log(  Status  );
+                    if(  Status === 'success'  ){
+                        global.location.reload(  true  );
+                    }
                   }
                 }
               );
@@ -179,138 +182,8 @@ APP.meetings = (  function(  global, app  ){
         day = dateTimeString[  2  ];
         hrMin = dateTimeString[  3  ];
         dateTimeString = yr + '-' + mth + '-' + day + 'T' + hrMin;
-        // + ':00Z-05:00';
-        // dateTimeString = yr + ' ' + mth + ' ' + day + ' ' + hrMin;
-
-        // for(  var j = 0; j < dateTimeString.length; j++  ){
-        //   // if(  j !== dateTimeString.length -1  ){
-        //     dateTimeArr.push(  dateTimeString[  j  ]  );
-        //   //}
-          // else if(  j === (  dateTimeString.length - 1  )  ){
-          //   var hrsMins = dateTimeString[  j  ].split(  ':'  );
-          //   var hrs = hrsMins[  0  ];
-          //   var mins = hrsMins[  1  ];
-          //   dateTimeArr.push(  hrs  );
-          //   dateTimeArr.push(  mins  );
-          // }
-        //}
         return dateTimeString;
   }
-
-//****************DEPRECATED/OBSOLETE*******************************//
-  // function addMeeting(){
-  //   /*****Begin variable declarations******/
-  //   var $form = $(  '#create-meeting'  ),
-  //       $startDate = $(  'input[name="start-date"]', $form  ),
-  //       $startTime = $(  'input[name="start-time"]', $form  ),
-  //       $endDate = $(  'input[name="end-date"]', $form  ),
-  //       $endTime = $(  'input[name="end-time"]', $form  ),
-  //       $location = $(  'input[name="meeting-location"]', $form  ),
-  //       $title = $(  'input[name="meeting-title"]', $form  ),
-  //       organizerEmail = 'bFranklin@1776.org',
-  //       uid = '{313EB79D-4574-494F-BB53-EC31068A3FAB}',
-  //       webURL = app.getWebUrl(),
-  //       sequence = 1,
-  //       utcDateStamp,
-  //       yearNow,
-  //       monthNow,
-  //       dayNow,
-  //       hrMinNow,
-  //       titleVal = $(  'input[name="meeting-title"]'  ).val(),
-  //       dateStartVal = $startDate.val(),
-  //       timeStartVal = $startTime.val(),
-  //       dateEndVal = $endDate.val(),
-  //       timeEndVal = $endTime.val(),
-  //       location = $location.val(),
-  //       startYear,
-  //       startMonth,
-  //       startDay,
-  //       startHrMin,
-  //       endYear,
-  //       endMmonth,
-  //       endDay,
-  //       endHrMin,
-  //       utcDateStart,
-  //       utcDateEnd,
-  //
-  //
-  //       /*****End variable declarations******/
-  //
-  //       /****Begin build UTC now date********/
-  //       utcDateStamp = new Date(  Date.now()  );
-  //       utcDateStamp = utcDateStamp.toString();
-  //       utcDateStamp = utcDateStamp.split(  ' '  );
-  //       yearNow = utcDateStamp[  3  ];
-  //       for(  var month in monthMap  ){
-  //         if(  utcDateStamp[  1  ] === month  ){
-  //           monthNow = monthMap[  month  ];
-  //         }
-  //       }
-  //       dayNow = utcDateStamp[  2  ];
-  //       hrMinNow = utcDateStamp[  4  ];
-  //       utcDateStamp = yearNow + '-' + monthNow + '-' + dayNow + 'T' + hrMinNow + '.000';
-  //       /***End build UTC now date**********/
-  //
-  //       /****Begin build UTC start date******/
-  //       dateStartVal = makeDateTimeString(  dateStartVal, timeStartVal  );
-  //       startYear = dateStartVal[  0  ];
-  //       startMonth = dateStartVal[  1  ];
-  //       startDay = dateStartVal[  2  ];
-  //       startHrMin = dateStartVal[  3  ];
-  //       utcDateStart = startYear + '-' + startMonth + '-' + startDay + 'T' + startHrMin + ':00.000';
-  //       console.log(  'UTC Date Start: ' + utcDateStart  );
-  //       /****End build UTC start date******/
-  //
-  //       /****Begin build UTC end date*****/
-  //       dateEndVal = makeDateTimeString(  dateEndVal, timeEndVal  );
-  //       endYear = dateEndVal[  0  ];
-  //       endMonth = dateEndVal[  1  ];
-  //       endDay = dateEndVal[  2  ];
-  //       endHrMin = dateEndVal[  3  ];
-  //       utcDateEnd = endYear + '-' + endMonth + '-' + endDay + 'T' + endHrMin + ':00.000';
-  //       console.log(  'UTC Date End: ' + utcDateEnd  );
-  //       /****End build UTC end date*****/
-  //
-
-  //   // $().SPServices(
-  //   //   {
-  //   //     operation         :     'AddMeeting',
-  //   //     webURL            :     webURL,
-  //   //     uid               :     uid,
-  //   //     location          :     location, //ASK BOB WHAT THIS IS
-  //   //     organizerEmail    :     organizerEmail,
-  //   //     sequence          :     sequence, //ASK BOB WHAT THIS IS
-  //   //     utcDateStamp      :     utcDateStamp,
-  //   //     title             :     titleVal,
-  //   //     utcDateStart      :     utcDateStart,
-  //   //     utcDateEnd        :     utcDateEnd,
-  //   //     nonGregorian      :     false,
-  //   //     completefunc      :     function(  xData, Status  ){
-  //   //       console.log(  Status  );
-  //   //       console.log(  xData.responseText  );
-  //   //     }
-  //   //   }
-  //   // );
-  //   /*
-  //   *SPServices 'AddMeeting' method parameters:
-  //     organizerEmail        :     Email address ('email_address@domain.ext') of the organizer
-  //     uid                   :     GUID for the calendar
-  //     sequence              :     An integer used to determine the ordering of updates in case they arrive out of sequence.
-  //                                 Updates with a lower-than-current sequence are discarded.
-  //                                 If the sequence is the same as the current sequence, the latest update will be applied.
-  //     utcDateStamp          :     The date and time that the instance of the iCalendar object was created.
-  //     title                 :     The title (subject) of the meeting.
-  //     location              :     The location of the meeting.
-  //     utcDateStart          :     The start date and time for the meeting, expressed in UTC.
-  //     utcDateEnd            :     The end date and time for the meeting, expressed in Coordinated Universal Time (UTC).
-  //     nonGregorian          :     true if the calendar is set to a format other than Gregorian; otherwise, false.
-  //                                 Setting this parameter to true puts the following message in the summary data
-  //                                 of the meeting workspace: "This meeting was created using a calendar and scheduling program
-  //                                 that only supports series updates to the Meeting Workspace.
-  //                                 Changes you make to individual occurrences of meetings in
-  //                                 that program will not appear in the workspace."
-  //   */
-  // }
 
   function initMeeting(){
     console.log(  "Meeting initiated."  );
@@ -320,7 +193,6 @@ APP.meetings = (  function(  global, app  ){
   /*******************RETURN PUBLIC API*********************/
   return(
     {
-      // onSubmit        :       onSubmit
       initMeeting        :       initMeeting
     }
   )
